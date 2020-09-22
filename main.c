62c251da3c329a5125457202e0b50ef570f3118d
NamDo <namdo10121999@gmail.com>
2020-09-22 23:17:33 +0700

    Update function: register and login

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 
 #include "linkedlist.h"
 
+void readFileToList(linkedList *list);
+void appendDataToFile(FILE *file, nodeValType val);
+void writeListToFile(linkedList *list);
+
+// Node *getUser(linkedList *list, nodeValType user);
+Node *getUser(linkedList *list, char username[20]);
+
+void handleRegister(linkedList *list);
+void handleSignIn(linkedList *list);
+
 void displayMenu()
 {
   printf("\n \n---------------------------------------\n");
@@ -16,6 +26,103 @@ void displayMenu()
   printf("Your choice (1-4), other to quit: \n");
 }
 
+// Handle file
+void handleRegister(linkedList *list)
+{
+  nodeValType newUser;
+
+  // GET user's inputted info
+  printf("\n---------Register---------\n");
+  printf("Username: ");
+  scanf("%s", newUser.username);
+  printf("Password: ");
+  scanf("%s", newUser.password);
+
+  // Initial user's state = idle (2)
+  newUser.status = 2;
+
+  // Check if user is exist or not
+  if (getUser(list, newUser.username) != NULL)
+  {
+    printf("Account existed");
+    return;
+  }
+
+  // Insert new user to list
+  insertEnd(list, newUser);
+
+  // Update file
+  writeListToFile(list);
+
+  // After all, tell the user
+  printf("Successful registration. Activation required.");
+}
+
+// Handle file
+void handleSignIn(linkedList *list)
+{
+  int inputTime = 0;
+  char username[20];
+  char password[20];
+  Node *user;
+
+  // GET user's username
+  printf("\n---------Sign in---------\n");
+  printf("Username: ");
+  scanf("%s", username);
+
+  // Get user based on username
+  user = getUser(list, username);
+  if (user == NULL)
+  {
+    printf("Cannot find account\n");
+  }
+
+  // Get user's password
+  while (inputTime < 3)
+  {
+    printf("Password: ");
+    scanf("%s", password);
+
+    // If password is NOT wrong
+    if (strcmp(user->val.password, password) == 0)
+      break;
+
+    // Else if password IS WRONG
+    printf("Password is incorrect\n");
+    inputTime++;
+  }
+
+  if (inputTime == 3)
+  {
+    printf("Password is incorrect. Account is blocked\n");
+    return;
+  }
+
+  // Check if user is BLOCKED or NOT
+  if (user->val.status == 0)
+  {
+    printf("Account is blocked\n");
+    return;
+  }
+
+  // After all, Hello User!
+  printf("Hello %s\n", username);
+}
+
+// Utils file
+Node *getUser(linkedList *list, char username[20])
+{
+  Node *tmp = list->root;
+  while (tmp != NULL)
+  {
+    if (strcmp(tmp->val.username, username) == 0)
+      return tmp;
+    tmp = tmp->next;
+  }
+  return NULL;
+}
+
 int getUserChoice()
 {
   int choice = 0;
@@ -23,61 +130,6 @@ int getUserChoice()
   displayMenu();
   scanf("%d", &choice);
   return choice;
-}
-
-/* BINARY FILE */
-
-void readBinaryFileToList(linkedList *list)
-{
-  nodeValType node;
-
-  // Open account file for reading
-  FILE *file;
-  if ((file = fopen("account.bin", "rb+")) == NULL)
-  {
-    printf("File not found!\n");
-    return;
-  }
-
-  // Read to list
-  while (!feof(file))
-  {
-    fscanf(file, "%s %s %d", node.username, node.password, &node.status);
-    if (feof(file))
-      break;
-    insertEnd(list, node);
-  }
-
-  // Close file
-  fclose(file);
-}
-
-void appendDataToBinaryFile(FILE *file, nodeValType val)
-{
-  printf("writting:..");
-  fprintf(file, "%s %s %d\n", val.username, val.password, val.status);
-}
-
-void writeListToBinaryFile(linkedList *list)
-{
-  // Open account file for reading
-  FILE *file;
-  if ((file = fopen("account.bin", "ab")) == NULL)
-  {
-    printf("File not found!\n");
-    return;
-  }
-
-  // Loop for list to write node to file
-  Node *tmp = list->root;
-  while (tmp != NULL)
-  {
-    appendDataToBinaryFile(file, tmp->val);
-    tmp = tmp->next;
-  }
-
-  // Close file
-  fclose(file);
 }
 
 /* PLAIN TEXT FILE */
@@ -96,9 +148,12 @@ void readFileToList(linkedList *list)
   // Read to list
   while (!feof(file))
   {
-    fscanf(file, "%s %s %d", node.username, node.password, &node.status);
-    if (feof(file))
-      break;
+    int scan = fscanf(file, "%s %s %d\n", node.username, node.password, &node.status);
+    printf("Scan: %d\n", scan);
+    printf("Pos: %d\n", ftell(file));
+
+    // if (feof(file))
+    //   break;
     insertEnd(list, node);
   }
 
@@ -116,7 +171,7 @@ void writeListToFile(linkedList *list)
 {
   // Open account file for reading
   FILE *file;
-  if ((file = fopen("account2.txt", "a")) == NULL)
+  if ((file = fopen("account.txt", "w")) == NULL)
   {
     printf("File not found!\n");
     return;
@@ -136,72 +191,44 @@ void writeListToFile(linkedList *list)
 
 int main()
 {
-  // do
-  // {
-  //   // Display menu and get user's choice
-  //   int choice = getUserChoice();
+  linkedList list;
+  createSingleList(&list);
+  readFileToList(&list);
 
-  //   // Do task
-  //   switch (choice)
-  //   {
-  //   case 1:
-  //     printf("Choice 1");
-  //     // Handle register
-  //     break;
-  //   case 2:
-  //     printf("Choice 2");
-  //     // Handle signIn
-  //     break;
-  //   case 3:
-  //     printf("Choice 3");
-  //     // Handle search
-  //     break;
-  //   case 4:
-  //     printf("Choice 4");
-  //     // Handle signOut
-  //     break;
-  //   default:
-  //     // Exit program
-  //     return 0;
-  //   }
+  do
+  {
+    // Display menu and get user's choice
+    int choice = getUserChoice();
 
-  // } while (1);
+    // Do task
+    switch (choice)
+    {
+    case 1:
+      // Handle register
+      handleRegister(&list);
+      break;
+    case 2:
+      handleSignIn(&list);
+      // Handle signIn
+      break;
+    case 3:
+      printf("Choice 3");
+      // Handle search
+      break;
+    case 4:
+      printf("Choice 4");
+      // Handle signOut
+      break;
+    default:
+      // Exit program
+      return 0;
+    }
 
-  // displayMenu();
-  // nodeValType newVal;
-  // nodeValType newVal2;
-  // nodeValType newVal3;
-  // strcpy(newVal.username, "do quang nam");
-  // strcpy(newVal.password, "12345");
-  // newVal.status = 1;
+  } while (1);
 
-  // strcpy(newVal2.username, "do quang nam");
-  // strcpy(newVal2.password, "12345");
-  // newVal2.status = 2;
-  // strcpy(newVal3.username, "do quang nam");
-  // strcpy(newVal3.password, "12345");
-  // newVal3.status = 2;
+  printSingleList(&list);
+  // printSingleList(&list_bin);
 
-  // // strcpy(newVal.username, "do quang nam");
-  // // strcpy(newVal.password, "12345");
-  // // newVal.status = 1;
-
-  linkedList *list = createSingleList();
-  readFileToList(list);
-
-  // printf("Programing...");
-  // insertBegin(list, newVal);
-  // insertEnd(list, newVal2);
-  // insertBegin(list, newVal3);
-  // insertBegin(list, newVal2);
-  // insertBegin(list, 1);
-  // insertBegin(list, 4);
-  // printSingleList(list);
-  writeListToBinaryFile(list);
-
-  linkedList *list_bin = createSingleList();
-  readBinaryFileToList(list_bin);
-
-  printSingleList(list_bin);
+  // writeListToFile(&list_bin);
   return 0;
 }
