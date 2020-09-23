635f0444bdbe69122936c782586ac45d95200128
NamDo <namdo10121999@gmail.com>
2020-09-23 16:20:04 +0700

    Add other handler function

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 
 #include "linkedlist.h"
 
+/* GLOBAL VAR*/
+Node *currentSignInUser = NULL;
+
 void readFileToList(linkedList *list);
 void appendDataToFile(FILE *file, nodeValType val);
 void writeListToFile(linkedList *list);
 
 // Node *getUser(linkedList *list, nodeValType user);
 Node *getUser(linkedList *list, char username[20]);
+void updateUser(linkedList *list, nodeValType user, char option[10]);
 
 void handleRegister(linkedList *list);
 void handleSignIn(linkedList *list);
@@ -20,10 +24,21 @@ void displayMenu()
   printf("USER MANAGEMENT PROGRAM\n");
   printf("---------------------------------------\n");
   printf("1. Register\n");
-  printf("2. Sign in\n");
-  printf("3. Search\n");
-  printf("4. Sign out\n");
+  printf("2. Activate\n");
+  printf("3. Sign in\n");
+  printf("4. Search\n");
+  printf("5. Change password\n");
+  printf("6. Sign out\n");
   printf("Your choice (1-4), other to quit: \n");
+}
+
+int getUserChoice()
+{
+  int choice = 0;
+
+  displayMenu();
+  scanf("%d", &choice);
+  return choice;
 }
 
 // Handle file
@@ -58,8 +73,8 @@ void handleRegister(linkedList *list)
   printf("Successful registration. Activation required.");
 }
 
-// Handle file
-void handleSignIn(linkedList *list)
+// Return signin successfully user
+Node *userAuth(linkedList *list)
 {
   int inputTime = 0;
   char username[20];
@@ -67,7 +82,6 @@ void handleSignIn(linkedList *list)
   Node *user;
 
   // GET user's username
-  printf("\n---------Sign in---------\n");
   printf("Username: ");
   scanf("%s", username);
 
@@ -96,18 +110,184 @@ void handleSignIn(linkedList *list)
   if (inputTime == 3)
   {
     printf("Password is incorrect. Account is blocked\n");
-    return;
+    // BLOCK USER
+    user->val.status = 0;
+    updateUser(list, user->val, "status");
+    return NULL;
   }
 
   // Check if user is BLOCKED or NOT
   if (user->val.status == 0)
   {
     printf("Account is blocked\n");
+    return NULL;
+  }
+
+  // Return signed in user
+  currentSignInUser = user;
+  return user;
+}
+
+// Handle file
+void handleSignIn(linkedList *list)
+{
+  printf("\n---------Sign in---------\n");
+
+  // user authentication
+  Node *signedInUser = userAuth(list);
+
+  // After all, Hello User!
+  if (signedInUser != NULL)
+    printf("Hello %s\n", signedInUser->val.username);
+}
+
+// Handle file
+void handleActivate(linkedList *list)
+{
+  int inputTime = 0;
+  char inputCode[10];
+
+  printf("\n---------Activate account---------\n");
+
+  // user authentication
+  Node *signedInUser = userAuth(list);
+  if (signedInUser == NULL)
+    return;
+
+  // GET user's inputted code
+  while (inputTime < 5)
+  {
+    printf("Code: ");
+    scanf("%s", inputCode);
+
+    // If activate code is NOT wrong
+    if (strcmp("LTM121216", inputCode) == 0)
+    {
+      printf("Account is activated");
+      signedInUser->val.status = 1;
+      updateUser(list, signedInUser->val, "status");
+      return;
+    }
+
+    // Else if password IS WRONG
+    printf("Activation code is incorrect\n");
+    inputTime++;
+  }
+
+  // Block user if 5 times wrong
+  if (inputTime == 5)
+  {
+    printf("Activation code is incorrect. Account is blocked\n");
+    // BLOCK USER
+    signedInUser->val.status = 0;
+    updateUser(list, signedInUser->val, "status");
+    return;
+  }
+}
+
+// Handle file
+void handleSearch(linkedList *list)
+{
+  char username[20];
+
+  // Check if user is signed in or not
+  if (currentSignInUser == NULL)
+  {
+    printf("Account is not sign in");
     return;
   }
 
-  // After all, Hello User!
-  printf("Hello %s\n", username);
+  // Get user's input
+  printf("\n---------Search---------\n");
+  printf("Username: ");
+  scanf("%s", username);
+
+  // Check account exist
+  Node *user = getUser(list, username);
+  if (user == NULL)
+  {
+    printf("Cannot find account");
+    return;
+  }
+
+  // Log to user account's status
+  switch (user->val.status)
+  {
+  case 0:
+    printf("Account is blocked");
+    break;
+  case 1:
+    printf("Account is active");
+    break;
+  case 2:
+    printf("Account is not activated");
+    break;
+  default:
+    break;
+  }
+}
+
+// Handle file
+void handleChangePassword(linkedList *list)
+{
+  char newPassword[20];
+  nodeValType user;
+
+  // Check current user is signed in or NOT
+  if (currentSignInUser == NULL)
+  {
+    printf("Account is not sign in");
+    return;
+  }
+
+  // Get user's input
+  printf("\n---------Change password---------\n");
+  printf("Username: ");
+  scanf("%s", user.username);
+  printf("Password: ");
+  scanf("%s", user.password);
+  printf("NewPassword: ");
+  scanf("%s", newPassword);
+
+  // Check current password is NOT WRONG
+  if (strcmp(currentSignInUser->val.password, user.password) != 0)
+  {
+    printf("Current password is incorrect. Please try again");
+    return;
+  }
+
+  // Change password and save
+  strcpy(user.password, newPassword);
+  updateUser(list, user, "password");
+  printf("Password is changed");
+}
+
+void handleLogOut(linkedList *list)
+{
+  char username[20];
+
+  // Get user's input
+  printf("\n---------Logging out---------\n");
+  printf("Username: ");
+  scanf("%s", username);
+
+  // Check current user is signed in or NOT
+  if (currentSignInUser == NULL || strcmp(currentSignInUser->val.username, username) != 0)
+  {
+    printf("Account is not sign in");
+    return;
+  }
+
+  // Check user's account is exist
+  if (getUser(list, username) == NULL)
+  {
+    printf("Cannot find account");
+    return;
+  }
+
+  // Logging out user
+  currentSignInUser = NULL;
+  printf("Goodbye %s", username);
 }
 
 // Utils file
@@ -123,13 +303,24 @@ Node *getUser(linkedList *list, char username[20])
   return NULL;
 }
 
-int getUserChoice()
+// Option is: "password", "name", "status"
+void updateUser(linkedList *list, nodeValType user, char option[10])
 {
-  int choice = 0;
+  // Find user based on name
+  Node *updateUser = getUser(list, user.username);
 
-  displayMenu();
-  scanf("%d", &choice);
-  return choice;
+  // Update user's password and/or status
+  if (strcmp(option, "password") == 0)
+  {
+    strcpy(updateUser->val.password, user.password);
+  }
+  if (strcmp(option, "status") == 0)
+  {
+    updateUser->val.status = user.status;
+  }
+
+  // Write to file
+  writeListToFile(list);
 }
 
 /* PLAIN TEXT FILE */
@@ -149,11 +340,6 @@ void readFileToList(linkedList *list)
   while (!feof(file))
   {
     int scan = fscanf(file, "%s %s %d\n", node.username, node.password, &node.status);
-    printf("Scan: %d\n", scan);
-    printf("Pos: %d\n", ftell(file));
-
-    // if (feof(file))
-    //   break;
     insertEnd(list, node);
   }
 
@@ -163,7 +349,6 @@ void readFileToList(linkedList *list)
 
 void appendDataToFile(FILE *file, nodeValType val)
 {
-  printf("writting:..");
   fprintf(file, "%s %s %d\n", val.username, val.password, val.status);
 }
 
@@ -200,7 +385,7 @@ int main()
     // Display menu and get user's choice
     int choice = getUserChoice();
 
-    // Do task
+    // Do tasks
     switch (choice)
     {
     case 1:
@@ -208,16 +393,24 @@ int main()
       handleRegister(&list);
       break;
     case 2:
-      handleSignIn(&list);
-      // Handle signIn
+      // Handle active account
+      handleActivate(&list);
       break;
     case 3:
-      printf("Choice 3");
-      // Handle search
+      // Handle signIn
+      handleSignIn(&list);
       break;
     case 4:
-      printf("Choice 4");
-      // Handle signOut
+      // Handle search
+      handleSearch(&list);
+      break;
+    case 5:
+      // Handle change password
+      handleChangePassword(&list);
+      break;
+    case 6:
+      // Handle logging out
+      handleLogOut(&list);
       break;
     default:
       // Exit program
@@ -226,9 +419,6 @@ int main()
 
   } while (1);
 
-  printSingleList(&list);
-  // printSingleList(&list_bin);
-
-  // writeListToFile(&list_bin);
+  // printSingleList(&list);
   return 0;
 }
